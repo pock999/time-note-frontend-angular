64839a3ed471740d51c69214dfeb9881be992823
Rickh <hpx210650@gmail.com>
2022-10-08 22:49:12 +0800

    🎉 init:

---
+++ src/app/account/account.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AccountRoutingModule } from './account-routing.module';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ProfileComponent } from './profile/profile.component';
+
+import { AccountService } from './account.service';
+
+@NgModule({
+  declarations: [
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent
+  ],
+  exports: [
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent
+  ],
+  imports: [
+    CommonModule,
+    AccountRoutingModule
+  ],
+  providers: [
+    AccountService,
+  ],
+})
+export class AccountModule { }
