8cddb07f495ef616ea42d47008c68f153646b695
Rickh <hpx210650@gmail.com>
2022-10-08 22:53:26 +0800

    🎉 init: add need file

--- src/app/account/account.module.ts
+++ src/app/account/account.module.ts
@@ -6,7 +6,7 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ProfileComponent } from './profile/profile.component';
 
-import { AccountService } from './account.service';
+import { AccountService } from './services/account.service';
 
 @NgModule({
   declarations: [
